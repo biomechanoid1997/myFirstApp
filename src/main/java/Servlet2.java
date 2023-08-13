c70b066e5112862afb67aaa9bd2da7b7ae7d6b78
biomechanoid <biomechanoid1997@yandex.ru>
2023-08-13 19:09:51 +0300

    ...

---
+++ src/main/java/Servlet2.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class Servlet2 extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        try{
+            writer.println("<h2>Hello from Servlet9</h2>");
+        }finally {
+            writer.close();
+        }
+    }
+}
